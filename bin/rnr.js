80e9cf275767cba59f8d5314e4cb27caf6192f3d
przemek-nowicki <nowicki.przemek@gmail.com>
2022-05-04 23:22:27 +0200

    read and write pid using file system

--- bin/rnr.js
+++ bin/rnr.js
@@ -2,7 +2,20 @@
 
 "use strict";
 
-console.log('Inside of RnR bin!')
-console.log(process);
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
+var args = process.argv[2];
 
+fs.readFile(path.join(os.tmpdir(), 'rnr.pid'), 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  const pid = parseInt(data);
+  if (pid > 0) {
+    process.kill(pid, "SIGPIPE");
+  } else {
+    console.error('pid not found in the storage');
+  }
+});
--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-'use strict';
+  'use strict';
 
-let cbToCall;
-const process = global.process
+  const fs = require('fs');
+  const path = require('path');
+  const os = require('os');
 
-const isProcessOk =  (process) => {
-  return process &&
-    typeof process === 'object' &&
-    typeof process.kill === 'function' &&
-    typeof process.pid === 'number' &&
-    typeof process.on === 'function'
-}
+  let cbToCall;
+  const process = global.process
 
-const reloadCbFn = (cb) => {
-  cbToCall = cb;
-};
+  const isProcessOk =  (process) => {
+    return process &&
+      typeof process === 'object' &&
+      typeof process.kill === 'function' &&
+      typeof process.pid === 'number' &&
+      typeof process.on === 'function'
+  }
 
-process.on('SIGPIPE', () => {
-  if (typeof cbToCall === 'function') {
-    cbToCall();  
+  const reloadCbFn = (cb) => {
+    cbToCall = cb;
+    fs.writeFile(path.join(os.tmpdir(), 'rnr.pid'), process.pid.toString(), function (err) {
+      if (err) return console.log(err);
+      console.log('rnr.pid saved successfully');
+    });
+  };
+
+  process.on('SIGPIPE', () => {
+    if (typeof cbToCall === 'function') {
+      cbToCall();  
+    } else {
+      console.error('SIGPIPE triggered but no callback provided to execute!');
+    }
+  });
+
+  if (!isProcessOk(process)) {
+    module.exports = () => {
+      return () => {}
+    }
   } else {
-    console.error('SIGPIPE triggered but no callback provided to execute!');
+    module.exports = reloadCbFn;
   }
-});
-
-if (!isProcessOk(process)) {
-  module.exports = () => {
-    return () => {}
-  }
-} else {
-  module.exports = reloadCbFn;
-}
